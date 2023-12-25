7f6ea663cc3cb87e03e439d31af51908d74496a1
ImLeonn <95400279+ImLeonn@users.noreply.github.com>
2023-12-25 13:13:42 +0700

    Delete Videos directory

--- Videos/lab1-main/src/Main.java
+++
@@ -1,9 +0,0 @@
-import vehicles.Vehicle;
-
-public class Main {
-    public static void main(String[] args) {
-        List<Vehicle> lst = new List<>();
-        String fileName = "CommandsList.txt";
-        lst.readFile(fileName);
-    }
-}
